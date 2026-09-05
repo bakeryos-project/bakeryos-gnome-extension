6cd36578a4e3906cdc587eb5badcad9c19fceba9
smtdfc <admin.smtdfc@gmail.com>
2026-09-05 16:40:02 +0700

    refactor: update extension URL and remove BakeryOSIndicator implementation

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,14 @@
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import St from 'gi://St';
 import { Extension, } from 'resource:///org/gnome/shell/extensions/extension.js';
-import { BakeryOSIndicator } from "./quickSettings.js";
+
 
 export default class BakeryOSExtension extends Extension {
     private _indicator: any = null;
     private _theme: any = null;
 
     enable() {
-        this._indicator = new BakeryOSIndicator();
-        Main.panel.statusArea.quickSettings!.addExternalIndicator(this._indicator);
+
         let stylesheet = this.dir.get_child('./styles/stylesheet.css');
         this._theme = St.ThemeContext.get_for_stage(global.stage).get_theme();
 
@@ -22,9 +21,7 @@ export default class BakeryOSExtension extends Extension {
     }
 
     disable() {
-        this._indicator.destroy();
-        this._indicator = null;
-
+      
         let stylesheet = this.dir.get_child('stylesheet.css');
         this._theme.unload_stylesheet(stylesheet);
         this._theme = null;
